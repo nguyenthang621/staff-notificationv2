fa91ed4b25572aaf48b63ec179fe1813ae66ef88
ndthang <ndthang@istt.com.vn>
2024-06-05 16:08:15 +0700

    update

--- src/main/java/com/istt/staff_notification_v2/security/SecurityConfig.java
+++ src/main/java/com/istt/staff_notification_v2/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.istt.staff_notification_v2.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.istt.staff_notification_v2.security.securityv2.CustomUserDetailsService;
 import com.istt.staff_notification_v2.security.securityv2.JwtAuthenticationEntryPoint;
@@ -66,5 +71,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		// Add our custom JWT security filter
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
